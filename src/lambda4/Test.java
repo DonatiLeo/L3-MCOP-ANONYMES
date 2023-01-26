23336fd459cc28938975e1cb670d0c2341384991
Leo Donati <leo.donati@unice.fr>
2023-01-26 15:35:00 +0100

    forEach

---
+++ src/lambda4/Test.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package lambda4;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Consumer;
+
+/**
+ * @author donati
+ *
+ */
+public class Test {
+
+	//Utilisation de foreach
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> tab = new ArrayList<>();
+		for (int i=0; i<10; i++)
+			tab.add(i);
+		
+		tab.sort( (o1, o2) -> {
+			if  (Math.sin(o2) < Math.sin(o1))
+				return 1;
+			else
+				return -1;
+		});
+		
+		/*//AVEC CLASSE ANONYE
+		tab.forEach(new Consumer<Integer>() {
+
+			@Override
+			public void accept(Integer t) {
+				System.out.println("pour  "+t+" on a "+Math.sin(t));
+			}
+		});
+		*/
+		//AVEC EXPRESSION LAMBDA
+		tab.forEach(
+			t -> { 
+				System.out.println("pour  "+t+" on a "+Math.sin(t));
+			}
+		);
+
+	}
+}
